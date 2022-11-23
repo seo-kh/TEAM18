c00ce508d5437efaad66ba9a55f40625e1f0b42d
서광현 <gospelseokh@gmail.com>
2022-11-23 10:49:11 +0900

    Initial Commit

---
+++ TEAM18/TEAM18App.swift
@@ -0,0 +1,17 @@
+//
+//  TEAM18App.swift
+//  TEAM18
+//
+//  Created by 서광현 on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TEAM18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
